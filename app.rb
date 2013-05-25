329765aa70891ba33e4470c305982af893ea9802
nefo-mi <nefo.mi@gmail.com>
2013-05-25 22:11:46 +0900

    fix

--- app.rb
+++ app.rb
@@ -3,13 +3,14 @@ $: << '.'
 
 require 'sinatra'
 require 'haml'
+require 'yaml'
 
 require 'lib/remocon'
 
 config_path = File.expand_path(File.dirname(__FILE__) + '/config.yml')
 config = YAML.load_file(config_path)
 
-remo = Remocon.new (
+remo = Remocon.new(
   config[:serial_port], 
   config[:serial_baudrate], 
   config[:serial_databit], 
