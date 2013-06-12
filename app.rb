5daa83121020bc082404b6e2ab7dd32dd9697f5c
nefo_mi <nefo.mi@gmail.com>
2013-06-12 23:51:19 +0900

    use configure method

--- app.rb
+++ app.rb
@@ -7,16 +7,18 @@ require 'yaml'
 
 require 'lib/remocon'
 
-config_path = File.expand_path(File.dirname(__FILE__) + '/config.yml')
-config = YAML.load_file(config_path)
+configure do
+  config_path = File.expand_path(File.dirname(__FILE__) + '/config.yml')
+  config = YAML.load_file(config_path)
 
-remo = Remocon.new(
-  config[:serial_port], 
-  config[:serial_baudrate], 
-  config[:serial_databit], 
-  config[:serial_stopbit], 
-  SerialPort::NONE
-)
+  $remo = Remocon.new(
+    config[:serial_port], 
+    config[:serial_baudrate], 
+    config[:serial_databit], 
+    config[:serial_stopbit], 
+    SerialPort::NONE
+  )
+end
 
 get '/', agent: %r{curl} do
   uri = request.host + (request.port == 80 ? '' : ":#{request.port.to_s}") 
@@ -29,13 +31,13 @@ end
 
 get '/' do
   @url = request.host + (request.port == 80 ? '' : ":#{request.port.to_s}")
-  @signals = remo.signals
+  @signals = $remo.signals
   haml :index
 end
 
 post '/', agent: %r{curl} do
   signal = params[:signal]
-  if ( remo.send_signal( signal ))
+  if ( $remo.send_signal( signal ))
     "send " + signal
   else
     signal + " not found"
